888f1300249caba100e59a1302551ccc09594c1d
haodong <haodong@ncamc.com.cn>
2022-03-15 21:21:43 +0800

    增加商品查询列表展示功能

--- gulimall-product/src/main/java/com/atguigu/gulimall/product/service/impl/CategoryServiceImpl.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/service/impl/CategoryServiceImpl.java
@@ -1,20 +1,23 @@
 package com.atguigu.gulimall.product.service.impl;
 
-import org.springframework.stereotype.Service;
-import java.util.Map;
-import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
-import com.baomidou.mybatisplus.core.metadata.IPage;
-import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.atguigu.common.utils.PageUtils;
 import com.atguigu.common.utils.Query;
-
 import com.atguigu.gulimall.product.dao.CategoryDao;
 import com.atguigu.gulimall.product.entity.CategoryEntity;
 import com.atguigu.gulimall.product.service.CategoryService;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 
 @Service("categoryService")
 public class CategoryServiceImpl extends ServiceImpl<CategoryDao, CategoryEntity> implements CategoryService {
+
 
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
@@ -24,6 +27,38 @@ public class CategoryServiceImpl extends ServiceImpl<CategoryDao, CategoryEntity
         );
 
         return new PageUtils(page);
+    }
+
+    @Override
+    public List<CategoryEntity> listWithTree() {
+        //1. 查出所有分类
+        List<CategoryEntity> entities = baseMapper.selectList(null);
+        //2. 组装成树形结构
+        //  找到一级分类
+        List<CategoryEntity> Level1Menus = entities.stream().filter((categoryEntity)
+                -> categoryEntity.getParentCid() == 0
+        ).map((menu)->{
+            menu.setChildren(getChildren(menu,entities));
+            return menu;
+        }).sorted((menu1,menu2)->{
+            return (menu1.getSort()==null?0:menu1.getSort())-(menu2.getSort()==null?0:menu2.getSort());
+        }).collect(Collectors.toList());
+
+        return Level1Menus;
+    }
+    //递归查找所有菜单的子菜单
+    private List<CategoryEntity> getChildren(CategoryEntity root, List<CategoryEntity> all) {
+        List<CategoryEntity> children = all.stream().filter((categoryEntity) -> {
+            return categoryEntity.getParentCid().equals(root.getCatId());
+        }).map((categoryEntity) -> {
+            //找到子菜单的list(递归)，set给categoryEntity
+            categoryEntity.setChildren(getChildren(categoryEntity, all));
+            return categoryEntity;
+        }).sorted((menu1, menu2)->{
+            //菜单的排序
+            return (menu1.getSort()==null?0:menu1.getSort())-(menu2.getSort()==null?0:menu2.getSort());
+        }).collect(Collectors.toList());
+        return children;
     }
 
 }
