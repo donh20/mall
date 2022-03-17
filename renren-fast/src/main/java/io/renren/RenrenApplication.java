569959f7e2efbeb903faff5d9b4dea4fb9bff9ce
haodong <haodong@ncamc.com.cn>
2022-03-17 21:35:37 +0800

    修改springcloud和springboot版本 否则renren-fast不兼容

--- renren-fast/src/main/java/io/renren/RenrenApplication.java
+++ renren-fast/src/main/java/io/renren/RenrenApplication.java
@@ -14,7 +14,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 
 @EnableDiscoveryClient
-//@SpringBootApplication
+@SpringBootApplication
 public class RenrenApplication {
 
 	public static void main(String[] args) {
