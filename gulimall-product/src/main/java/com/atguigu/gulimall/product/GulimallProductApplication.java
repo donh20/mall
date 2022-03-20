2fced08fb865732002c70e349e9c6ee3ddab375d
Dong Hao <haodong.donh@gmail.com>
2022-03-20 23:30:50 +0800

    增加商品服务的路由规则 修改springboot与springcloud版本,使得product应用与nacos兼容 修改junit的依赖

--- gulimall-product/src/main/java/com/atguigu/gulimall/product/GulimallProductApplication.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/GulimallProductApplication.java
@@ -3,6 +3,7 @@ package com.atguigu.gulimall.product;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /*
 * 1. 整合MyBatis-Plus
@@ -21,6 +22,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 *           2)告诉MyBatis-Plus
 *
 * */
+@EnableDiscoveryClient
 @MapperScan("com.atguigu.gulimall.product.dao")
 @SpringBootApplication
 public class GulimallProductApplication {
